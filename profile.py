318e25dc71e530ea73dd3f7d6cf380fdaed40684
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2020-07-15 20:41:05 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,11 +10,11 @@ node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh",
                               command="sudo apt update"))
-node.addService(rspec.Execute(shell="bin/sh",
+node.addService(rspec.Execute(shell="/bin/sh",
                                command="sudo apt install -y apache2"))
-node.addService(rspec.Execute(shell="bin/sh",
+node.addService(rspec.Execute(shell="/bin/sh",
                                command='sudo uwf allow in "Apache Full"'))
-node.addService(rspec.Execute(shell="bin/sh",
+node.addService(rspec.Execute(shell="/bin/sh",
                               command='sudo systemctl status apache2'))
 #Print the RSpec to the enclosing page
 portal.context.printRequestRSpec()
