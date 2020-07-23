529f1e9f9a7b603dcefc16adf5b0f0eee5c93ab2
patrickw42 <65424873+patrickw42@users.noreply.github.com>
2020-07-23 17:14:01 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -19,14 +19,14 @@ request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-for i in range(3):
+for i in range(2):
   if i == 0:
-    node = request.XenVM("ldapserver")    
+    node = request.XenVM("observer")          #this node runs the NFS server without public IP    
+    node.routable_control_ip = "true"         
   elif i == 1:
-    node = request.XenVM("nfsserver")
-  else:
-    node = request.XenVM("nfsclient")
-  node.routable_control_ip = "true"  
+    node = request.XenVM("webserver")         # this node runs the Apache server with a public IP
+    node.routable_control_ip = "false"
+  
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   iface = node.addInterface("if" + str(i))
   iface.component_id = "eth1"
@@ -34,4 +34,3 @@ for i in range(3):
   link.addInterface(iface)  
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
-
